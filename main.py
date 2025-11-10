2cb190e6239f594bf0d5324a2d2715d7b2efe56d
jere-cavs <jeremiasreyes18@hotmail.com>
2025-11-10 20:43:32 -0300

    Problematica de poo

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from Clases.prestamos_devoluciones import prestamos_devoluciones
 from Clases.usuarios import usuarios
 
 def registrar_libro():
-    titulo = input("Ingrese el título del libro: ")
+    titulo_libro = input("Ingrese el título del libro: ")
     ano_libro = int(input("Ingrese el año de publicación del libro: "))
     autor = input("Ingrese el autor del libro: ")
     genero = input("Ingrese el género del libro: ")
     editorial = input("Ingrese la editorial en la cuál fue piblicado el libro: ")
     isbn = input("Ingrese el isbn del libro")
-    libro = libros(titulo, ano_libro, autor, genero, editorial, isbn)
+    libro = libros(titulo_libro, ano_libro, autor, genero, editorial, isbn)
     return libro
 
 
@@ -24,27 +24,27 @@ def registrar_usuario():
     usuario = usuarios(nombre, telefono, direccion, altura, dni)
     return usuario
 
-def registrar_libros_prestados(usuarios, libros):
+def registrar_libros_prestados(usuario, libro):
     nombre_usuario = input("Ingrese su nombre: ")
-    usuario = next((c for c in usuarios if c.nombre == nombre_usuario), None)
+    usuario = next((c for c in usuario if c.nombre == nombre_usuario), None)
     if not usuario:
         print("No se ah encontrado el usuario")
         return
     
     nombre_libro = input("Ingrese el nombre del libro a prestar: ")
-    libro = next((l for l in libros if l.nombre == nombre_libro and l.disponible), None)
-    if not libro:
+    bucle_libro = next((l for l in libro if l.titulo_libro == nombre_libro and l.disponible), None)
+    if not bucle_libro:
         print("No se ah encontrado el libro o el libro no se encuentra disponible")
         return
     
-    prestamo = prestamos_devoluciones(usuario, libro)
+    prestamo = prestamos_devoluciones(usuario, bucle_libro)
     usuario.registrar_prestamo(prestamo)
-    libro.disponible = False
+    bucle_libro.disponible = False
     print("El préstamo ah sido registrado")
 
-def registrar_devoluciones(usuarios, libros):
+def registrar_devoluciones(usuario, libro):
     nombre_usuario = input("Ingrese su nombre: ")
-    usuario = next((u for u in usuarios if u.nombre == nombre_usuario), None)
+    usuario = next((u for u in usuario if u.nombre == nombre_usuario), None)
     if not usuario:
         print("El usuario ingresado no se encuentra registrado")
         return
@@ -69,8 +69,8 @@ def mostrar_menu():
     print("7. Salir del programa")
 
 def main():
-    libros = []
-    usuarios = []
+    lista_libros = []
+    lista_usuarios = []
 
     while True:
         mostrar_menu()
@@ -79,29 +79,29 @@ def main():
         if opcion == "1":
             libro = registrar_libro()
             if libro:
-                libros.append(libro)
+                lista_libros.append(libro)
                 print("El libro ah sido registrado con éxito")
 
         elif opcion == "2":
             usuario = registrar_usuario()
             if usuario:
-                usuarios.append(usuario)
+                lista_usuarios.append(usuario)
                 print("El usuario ah sido registrado con éxito")
         
         elif opcion == "3":
-            registrar_libros_prestados(usuarios, libros)
+            registrar_libros_prestados(usuario, libro)
         
         elif opcion == "4":
-            registrar_devoluciones(usuarios, libros)
+            registrar_devoluciones(usuario, libro)
 
         elif opcion == "5":
-            for libro in libros:
-                print(libros.mostrar_dispo_libros())
+            for libro in lista_libros:
+                print(libro.mostrar_dispo_libros())
         
         elif opcion == "6":
-            for usuario in usuarios:
-                print(usuarios.mostrar_informacion())
-                for prestamo in prestamos_devoluciones:
+            for usuario in lista_usuarios:
+                print(usuario.mostrar_informacion())
+                for prestamo in usuario.prestamos:
                     print(prestamo.mostrar_info())
         
         elif opcion == "7":
